d5e5bb60936a3d9113f737b28a2ad1cb700e3bf6
Eduardo Straub <eduardo.straub1@gmail.com>
2016-06-03 11:50:56 -0400

    Se elimina todo
    Se elimina para agregar nuevo código.

--- models/model.tokenlog.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var tokenLog = new Schema({
-  token: {type: String, required: true, trim: true, unique: true},
-  date_created: {type: date, required: true, default: date.now, trim: true}
-});
-
-module.exports = mongoose.model('TokenLog', tokenLog);
