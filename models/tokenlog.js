4c7785a3148534c1de047b6995885d7e4da95d38
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-06-24 04:47:11 -0300

    Add files via upload

--- models/tokenlog.js
+++ models/tokenlog.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var tokenLog = new Schema({
-  token: {type: String, required: true, trim: true, unique: true},
-  fecha_creacion: {type: Date, required: true, default: Date.now, trim: true}
+    token: {type: String, required: true, trim: true, unique: true},
+    fecha_creacion: {type: Date, required: true, default: Date.now, trim: true}
 });
 
 module.exports = mongoose.model('TokenLog', tokenLog);
--- models/usuario.js
+++ models/usuario.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var usuario = new Schema({
-  usuario: {type: String, required: true, trim: true, unique: true},
-  nombre: {type: String, required: true, trim: true},
-  apellidos: {type: String, required: true, trim: true},
-  contraseña: {type: String, required: true, trim: true},
-  edad: {type: Number, required: true, trim: true},
-  nivel_militar: {type: String, required: true, trim: true},
-  habilitado_para_usar_app: {type: Boolean, required: true, trim: true}
+    usuario: {type: String, required: true, trim: true, unique: true},
+    nombre: {type: String, required: true, trim: true},
+    apellidos: {type: String, required: true, trim: true},
+    contraseña: {type: String, required: true, trim: true},
+    edad: {type: Number, required: true, trim: true},
+    nivel_militar: {type: String, required: true, trim: true},
+    habilitado_para_usar_app: {type: Boolean, required: true, trim: true}
 });
 
 module.exports = mongoose.model('Usuario', usuario);
