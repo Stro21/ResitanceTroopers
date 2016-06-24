46028144394cf0532bdbf2593f0bcfb5261427e4
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-06-24 10:16:28 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var key = require("./functions/key").key();
 //configurar app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressJWT({secret: key}).unless({path: ['/login', '/usuarios/ingresar']}));
+app.use(expressJWT({secret: key}).unless({path: ['/','/login', '/usuarios/ingresar']}));
 app.use(api.validar_token);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('/public'));
@@ -24,11 +24,11 @@ app.use(express.static('/public'));
 app.get("/", api.main);
 //obtener usuario por id
 app.get("/usuarios/:id", api.obtener_usuario_por_id);
-//obtener todos los usuarios api.obtener_usuarios
+//obtener todos los usuarios
 app.get("/usuarios", api.obtener_usuarios);
 //obtener usuario por cuenta
 app.get("/usuarios/cuenta/:usuario", api.obtener_usuario_por_cuenta);
-//ingresar nuevo usuario forma
+//ingresar nuevo usuario
 app.post("/usuarios/ingresar", api.ingresar_nuevo_usuario);
 //borrar todos los usuarios
 app.delete("/usuarios/vaciar", api.borrar_todos_los_usuarios);
@@ -36,10 +36,24 @@ app.delete("/usuarios/vaciar", api.borrar_todos_los_usuarios);
 app.delete("/usuarios/:id", api.borrar_usuario_por_id);
 //modificar usuario por id
 app.put("/usuarios/:id", api.modificar_usario_por_id);
+
+//ingresar nuevo batallon
+app.post("/batallones/ingresar", api.ingresar_nuevo_batallon);
+//borrar batallon por id
+app.delete("/batallones/:id", api.borrar_batallon_por_id);
+//modificar batallon por id
+app.put("/batallones/:id", api.modificar_batallon_por_id);
+//obtener todos los batallon
+app.get("/batallones", api.obtener_batallones);
+//borrar batallon por id
+app.get("/batallones/:id", api.obtener_batallon_por_id);
+//atacar a batallon por id
+app.post("/batallones/atacar", api.atacar_a_batallon_por_id);
+
 //login
 app.post("/login", api.login);
 
 //escuchando en puerto
 app.listen(app.get('port'), function() {
-  console.log('Corriendo en putero: ', app.get('port'));
+  console.log('Escuchando en putero: ', app.get('port'));
 });
