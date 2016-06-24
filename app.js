204dfeb82ccb821bdbe79d0290b8f9c3d824c770
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-06-24 04:46:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,57 +2,39 @@ var express = require("express");
 var app = express();
 var http = require("http");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var server = http.createServer(/*handleRequestTextPlain*/);
+var server = http.createServer();
 var expressJWT = require("express-jwt");
-var db = "  mongodb://<Stro>:<Hola1234>@ds023064.mlab.com:23064/heroku_x76mjpd2";
-var key = "keydetesteo";
 var port = 8000;
 
 //MIS ARCHIVOS
-var api = require('./routes/controllers/api');
-
-mongoose.connect(db);
+var api = require('./routes/controllers/api'); 
+var key = require("./functions/key").key();
 
 //configurar app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(expressJWT({secret: key}).unless({path: ['/login']}));
+app.use(expressJWT({secret: key}).unless({path: ['/login', '/usuarios/ingresar']}));
+app.use(api.validar_token);
 
 //******************************LLAMADAS***********************************************************************
 
 //escuchando en puerto
 app.listen(port, api.escuchando(port));
-
 //pagina inicial
-app.get("/", api.main);
-
+app.get("/usuarios/main", api.main);
 //obtener usuario por id
 app.get("/usuarios/:id", api.obtener_usuario_por_id);
-
-//obtener todos los usuarios
+//obtener todos los usuarios api.obtener_usuarios
 app.get("/usuarios", api.obtener_usuarios);
-
 //obtener usuario por cuenta
 app.get("/usuarios/cuenta/:usuario", api.obtener_usuario_por_cuenta);
-
-//ingresar nuevo usuario forma 1 (por parte o por elemento)
-app.post("/ingresar", api.ingresar_nuevo_usuario);
-
+//ingresar nuevo usuario forma
+app.post("/usuarios/ingresar", api.ingresar_nuevo_usuario);
 //borrar todos los usuarios
 app.delete("/usuarios/vaciar", api.borrar_todos_los_usuarios);
-
 //borrar usuario por id
 app.delete("/usuarios/:id", api.borrar_usuario_por_id);
-
 //modificar usuario por id
 app.put("/usuarios/:id", api.modificar_usario_por_id);
-
 //login
 app.post("/login", api.login);
-
-/*function handleRequestTextPlain(request, response){
-  response.statusCode = 200;
-  response.setHeader('Contente-Type', 'text/plain');
-  response.end('Correcto' + request.url);
-};*/
