20573f53e1a97fe18cfda5e464e3efa9891b8751
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-06-24 10:18:16 -0300

    Add files via upload

---
+++ models/batallon.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var batallon = new Schema({
+    nombre: {type: String, required: true, trim: true, unique: true},
+    nombre_capitan: {type: String, required: true, trim: true},
+    latitud: {type: Number, required: true, trim: true},
+    longitud: {type: Number, required: true, trim: true},
+    cantidad_de_soldados_activos: {type: Number, required: true, trim: true}
+});
+
+module.exports = mongoose.model('Batallon', batallon);
