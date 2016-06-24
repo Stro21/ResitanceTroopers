4c08a85199d2f1db267ab27018b8cc5e614c1739
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-06-24 10:17:19 -0300

    Add files via upload

---
+++ usuario.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var usuario = new Schema({
+    usuario: {type: String, required: true, trim: true, unique: true},
+    nombre: {type: String, required: true, trim: true},
+    apellidos: {type: String, required: true, trim: true},
+    contraseña: {type: String, required: true, trim: true},
+    edad: {type: Number, required: true, trim: true},
+    nivel_militar: {type: String, required: true, trim: true},
+    habilitado_para_usar_app: {type: Boolean, required: true, trim: true}
+});
+
+module.exports = mongoose.model('Usuario', usuario);
