205685a4178b9902cd29e58ce6b0190934846869
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2016-07-01 06:38:12 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var key = require("./functions/key").key();
 //configurar app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressJWT({secret: key}).unless({path: ['/','/login', '/usuarios/ingresar']}));
+//app.use(expressJWT({secret: key}).unless({path: ['/login'}));
 app.use(api.validar_token);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('/public'));
@@ -29,7 +29,7 @@ app.get("/usuarios", api.obtener_usuarios);
 //obtener usuario por cuenta
 app.get("/usuarios/cuenta/:usuario", api.obtener_usuario_por_cuenta);
 //ingresar nuevo usuario
-app.post("/usuarios/ingresar", api.ingresar_nuevo_usuario);
+app.post("/usuarios", api.ingresar_nuevo_usuario);
 //borrar todos los usuarios
 app.delete("/usuarios/vaciar", api.borrar_todos_los_usuarios);
 //borrar usuario por id
@@ -38,7 +38,7 @@ app.delete("/usuarios/:id", api.borrar_usuario_por_id);
 app.put("/usuarios/:id", api.modificar_usario_por_id);
 
 //ingresar nuevo batallon
-app.post("/batallones/ingresar", api.ingresar_nuevo_batallon);
+app.post("/batallones", api.ingresar_nuevo_batallon);
 //borrar batallon por id
 app.delete("/batallones/:id", api.borrar_batallon_por_id);
 //modificar batallon por id
@@ -48,12 +48,14 @@ app.get("/batallones", api.obtener_batallones);
 //borrar batallon por id
 app.get("/batallones/:id", api.obtener_batallon_por_id);
 //atacar a batallon por id
-app.post("/batallones/atacar", api.atacar_a_batallon_por_id);
+app.post("/ataques", api.atacar_posicion);
+//obtener todos los ataques
+app.get("/ataques", api.obtener_ataques);
 
 //login
 app.post("/login", api.login);
 
 //escuchando en puerto
 app.listen(app.get('port'), function() {
-  console.log('Escuchando en putero: ', app.get('port'));
+  console.log('Escuchando en puerto: ', app.get('port'));
 });
